af491f3337bb89cadd37fce3941b430264e3c9a3
liyan123 <675672214@qq.com>
2020-10-30 20:36:04 +0800

    代码

---
+++ test16/Gift.java
@@ -0,0 +1,50 @@
+
+//public class Gift {
+//	public static void main(String args[]) {
+//		int a = 10;
+//		long b = 20L;
+//		a = (int)b;
+//		//b = a;
+//		System.out.println(a);
+//		System.out.println(b);
+//	}
+//}
+//public class Gift {
+//	public static void main(String args[]) {
+//		byte a = 10;
+//		byte b = 20;
+//		byte c = a + b);
+//		System.out.println(c);
+//	}
+//}
+/*public class Gift{
+    public static void main(String[] args) {
+		System.out.println(5/2);//2
+		System.out.println(5.0/2);//2.5
+		System.out.println(5/2.0);//2.5
+		System.out.println(5.0/2.0);//2.5
+	}
+}*/
+
+/*public class Gift {
+    public static void main(String[] args) {
+		System.out.println(10%3);//1
+		System.out.println(10%-3);//1
+		System.out.println(-10%3);//-1
+		System.out.println(-10%-3);//-1
+	}
+}*/
+
+public class Gift {
+	public static void main (String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(a == b);//flase
+		System.out.println(a != b);//true
+		System.out.println(a < b);//true
+		System.out.println(a > b);//flase
+		System.out.println(a >= b);//flase
+		System.out.println(a > b);//flase
+	}
+}
+
