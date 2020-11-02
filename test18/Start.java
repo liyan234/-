82cc4a92a29147973eeedd9565380b2818e2e060
liyan123 <675672214@qq.com>
2020-11-02 20:43:55 +0800

    代码

---
+++ test18/Start.java
@@ -0,0 +1,67 @@
+/*public class Start {
+	public static void main(String args[]) {
+		int a = 10;
+		System.out.println("a = " +a);
+		long b = 20L;//一般长整型数字后面加一个大写L，用来区别于整形
+		System.out.println("b = " +b);
+	}
+} */
+/*public class Start {
+	public static void main(String args[]) {
+		double a = 1.0;
+		double b = 2.0;
+		System.out.println("a/b = " +a/b);
+
+		int c = 1;
+		int d = 2;
+		System.out.println("c/d = " +c/d);
+
+		float e = 1.0f;
+		float f = 2.0f;//同样用作区分
+		System.out.println("e/f = " +e/f);
+	}
+}*/
+/*public class Start {
+	public static void main(String args[]) {
+		char a = '李';
+		System.out.println(a);
+		char b = 'p';
+		System.out.println(b);
+	}
+}*/
+/*public class Start {
+	public static void main(String args[]) {
+		byte a = 127;
+		System.out.println(a);
+		int b =  Integer.MAX_VALUE + 1;
+		System.out.println(b);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MAX_VALUE + 1);
+	}
+}*/
+
+/*public class Start {
+	public static void main(String args[]) {
+		short a = 1;
+		System.out.println(a);
+		boolean value = true;
+		System.out.println(value);
+	}
+}*/
+public class Start {
+	public static void main(String args[]) {
+		String s = "abcdef";
+		System.out.println(s);
+		
+		int a = 10;
+		System.out.println("a = " +a);
+		int b = 20;
+		System.out.println(a + b);
+		System.out.println("a+b = " +a+b);
+		System.out.println("a+b = " +(a+b));
+		System.out.println("a+b = " +a+ " " +b);
+
+		double c = 12.6;		
+		System.out.println("mmmmm=" +c);
+	}
+}
